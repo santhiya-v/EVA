f9291c0be45fa1b3665f140942b5248010cfb8b4
Santhiya Viswanathan <mailtosanthiya.v@gmail.com>
2020-05-11 08:34:49 +0530

    Modularizing the code

---
+++ S7/models/cifar.py
@@ -0,0 +1,137 @@
+import torch.nn as nn
+from .gbn import *
+
+dropout_value = 0.1
+num_splits = 2 
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        # CONVOLUTION BLOCK 1
+        self.convblock1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3, 3), padding=1, bias=False), #RF = 3
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value),
+
+            # Dilated Convolution
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=2, bias=False, dilation=2), #RF = 7
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(dropout_value),
+
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=1, bias=False), #RF = 9 
+            nn.ReLU(),
+            GhostBatchNorm(128, num_splits),
+            nn.Dropout(dropout_value),
+
+            nn.Conv2d(in_channels=128, out_channels=32, kernel_size=(1, 1), padding=0, bias=False), #RF = 9
+            nn.ReLU()
+
+        ) # output_size = 32
+
+        self.pool1 = nn.MaxPool2d(2, 2) # output_size = 16 RF = 10
+
+        # CONVOLUTION BLOCK 2
+        self.convblock2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=1, bias=False), #RF = 14
+            nn.ReLU(),
+            # GhostBatchNorm(64, num_splits),
+            nn.BatchNorm2d(64),
+            nn.Dropout(dropout_value),
+
+            # Depthwise separable convolution
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), padding=1, bias=False, groups=64), #RF = 18
+            nn.ReLU(),
+            # GhostBatchNorm(64, num_splits), 
+            nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(1, 1), padding=0, bias=False), #RF = 18
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Dropout(dropout_value),
+
+            nn.Conv2d(in_channels=128, out_channels=32, kernel_size=(1, 1), padding=0, bias=False), #RF = 18
+            nn.ReLU()
+        ) # output_size = 16
+        
+
+        self.pool2 = nn.MaxPool2d(2, 2) # output_size = 8 RF = 20
+
+        # CONVOLUTION BLOCK 3
+        self.convblock3 = nn.Sequential( 
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=2, bias=False), #RF = 28
+            nn.ReLU(),
+            # GhostBatchNorm(64, num_splits),
+            nn.BatchNorm2d(64),
+            nn.Dropout(dropout_value),
+
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=1, bias=False), #RF = 36
+            nn.ReLU(),
+            # GhostBatchNorm(128, num_splits),
+            nn.BatchNorm2d(128),
+            nn.Dropout(dropout_value),
+
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), padding=1, bias=False), #RF = 44
+            nn.ReLU(),
+            # GhostBatchNorm(256, num_splits),
+            nn.BatchNorm2d(256),
+            nn.Dropout(dropout_value),
+
+            nn.Conv2d(in_channels=256, out_channels=32, kernel_size=(1, 1), padding=0, bias=False), #RF = 44
+            nn.ReLU()
+        ) # output_size = 10
+
+        self.pool3 = nn.MaxPool2d(2, 2) # output_size = 5 RF = 48
+
+        # CONVOLUTION BLOCK 4
+        self.convblock4 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=2, bias=False), #RF = 64
+            nn.ReLU(),
+            # GhostBatchNorm(64, num_splits),
+            nn.BatchNorm2d(64),
+            nn.Dropout(dropout_value),
+
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=1, bias=False), #RF = 80
+            nn.ReLU(),
+            # GhostBatchNorm(128, num_splits),
+            nn.BatchNorm2d(128),
+            nn.Dropout(dropout_value),
+
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), padding=1, bias=False), #RF = 96
+            nn.ReLU(),
+            # GhostBatchNorm(256, num_splits),
+            nn.BatchNorm2d(256),
+            nn.Dropout(dropout_value),
+
+            # nn.Conv2d(in_channels=256, out_channels=64, kernel_size=(1, 1), padding=0, bias=False), 
+            # nn.ReLU(),
+            # nn.Dropout(dropout_value),
+
+        ) # output_size = 7
+
+
+        # OUTPUT BLOCK
+        self.gap = nn.Sequential(
+            nn.AvgPool2d(kernel_size=7)
+        ) # output_size = 1 RF = 120
+        self.convblock5 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=10, kernel_size=(1, 1), padding=0, bias=False), #RF = 
+        ) # output_size = 1 RF = 120
+
+        # self.fc1 = nn.Linear(1, 10) # output_size = 1
+
+
+
+    def forward(self, x):
+        x = self.convblock1(x)
+        x = self.pool1(x)
+        x = self.convblock2(x)
+        x = self.pool2(x)
+        x = self.convblock3(x)
+        x = self.pool3(x)
+        x = self.convblock4(x)
+        x = self.gap(x)
+        x = self.convblock5(x)
+        # x = self.fc1(x)
+        x = x.view(-1, 10)
+        return F.log_softmax(x, dim=-1)
---
+++ S7/validation.py
@@ -0,0 +1,57 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+
+test_losses = []
+test_acc = []
+
+def __test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_losses.append(test_loss)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    
+    test_acc.append(100. * correct / len(test_loader.dataset))
+
+# def class_performance(net, testloader, device):
+#   class_correct = list(0. for i in range(10))
+#   class_total = list(0. for i in range(10))
+#   with torch.no_grad():
+#       for data in testloader:
+#           images, labels = data
+#           images, labels = images.to(device), labels.to(device)
+#           outputs = net(images)
+#           _, predicted = torch.max(outputs, 1)
+#           c = (predicted == labels).squeeze()
+#           for i in range(4):
+#               label = labels[i]
+#               class_correct[label] += c[i].item()
+#               class_total[label] += 1
+#   for i in range(10):
+#       print('Accuracy of %5s : %2d %%' % (
+#           classes[i], 100 * class_correct[i] / class_total[i]))
+      
+# def predict_on_test(net, testloader, device ):
+#   dataiter = iter(testloader)
+#   images, labels = dataiter.next()
+#   images, labels = images.to(device), labels.to(device)
+#   # print images
+#   imshow(torchvision.utils.make_grid(images.cpu()))
+#   print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(64)))
+#   # print('GroundTruth: ', '%5s' % classes[labels])
+#   outputs = net(images)
+#   _, predicted = torch.max(outputs, 1)
+#   print('Predicted: ', '%5s' % ' '.join('%5s' % classes[predicted[j]] for j in range(64)))
