cc675785d664533e945ed59133c960767effdceb
santhiya-v <mailtosanthiya.v@gmail.com>
2020-07-25 10:18:36 +0530

    Adding mask images

--- S15/A/others/showcase_dataset.py
+++ S15/A/others/showcase_dataset.py
@@ -1,15 +1,18 @@
 
 from PIL import Image
+import cv2
 
 if __name__ == "__main__":
     new_im = Image.new('RGBA', (1460, 120))
     
     x_offset = 0
     for i in range(1,13):
-        im = Image.open(f'/home/santhiya/Work/projects/EVA/S15/showcase_images/depth/{i}.jpg')
+        im = Image.open(f'/home/santhiya/Work/projects/S15_prep/showcase_images/fg_bg_mask/{i}.jpg')
         im = im.resize((100,100))
         new_im.paste(im, (x_offset,0))
         x_offset += im.size[0] + 20
 
     new_im.show()
-    new_im.save('/home/santhiya/Work/projects/EVA/S15/showcase_images/depth.png')
+    new_im.save('/home/santhiya/Work/projects/S15_prep/showcase_images/fg_bg_mask.png')
+
+   
